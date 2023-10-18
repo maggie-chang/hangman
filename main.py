16ceaa0eabeb12fb5140d5f4fd64b4b4163bb7d5
maggie <144709218+maggie-chang@users.noreply.github.com>
2023-10-18 16:51:17 +0000

    update man

--- main.py
+++ main.py
@@ -8,48 +8,59 @@ WIDTH = 500
 HEIGHT = 300
 
 
-#def draw():
-    #screen.fill((255, 255, 255))
-    #spooky_word = get_random_spooky_word()
-    #print(spooky_word)
-    #screen.draw.text(spooky_word, (250, 150), color="red")
+# def draw():
+# screen.fill((255, 255, 255))
+# spooky_word = get_random_spooky_word()
+# print(spooky_word)
+# screen.draw.text(spooky_word, (250, 150), color="red")
 
 
 def get_random_spooky_word():
-    '''returns a random word from words.txt'''
+    """returns a random word from words.txt"""
     with open("words.txt") as file:
         return random.choice(list(file))[:-1]
-  
+
 
 def is_only_letters(word):
-    '''makes sure players' input is only letters'''
+    """makes sure players' input is only letters"""
     for letter in word:
-        if letter not in "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z":
-         return False
+        if (
+            letter
+            not in "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z"
+        ):
+            return False
     return True
 
-def word_place_clues(spooky_word, guessed_word, guessed_letter):
+
+def correct_word(
+    spooky_word,
+    guessed_word,
+):
     spooky_word = get_random_spooky_word
-    if guessed_word == spooky_word
+    if guessed_word == spooky_word:
         return print("you won!")
 
-    word_placement = []
-        for letter in guessed_letter:
-            if guessed_letter[] == len(spooky_word)[]
-                return len(spooky_word)[]
-            elif
 
-    
-    
+def clues_from_letter(spooky_word: str, guessed_letter):
+    clue = ""
+    guess_index = spooky_word.index(guessed_letter)
+
+    for i in range(len(spooky_word)):
+        if i == guess_index:
+            clue += guessed_letter
+        else:
+            clue += "*"
+
+    return clue
+
+print(clues_from_letter("halloween", "l"))
+
 
 
 
 print("this is hangman")
-print("enter letters and guess word")
-
 
 while True:
-
     spooky_word = get_random_spooky_word()
     num_letters = len(spooky_word)
     print(f"spooky word is {spooky_word}")
@@ -59,8 +70,6 @@ while True:
         letter_guess = ""
         while len(letter_guess) != int(max_wrong_guesses) or not is_only_letters:
             letter_guess = input()
-
-
 
 
 pgzrun.go()
