40a9d61d3a7ced10fd0107358214a227dd0de465
maggie <144709218+maggie-chang@users.noreply.github.com>
2023-10-18 16:19:16 +0000

    update hangman

--- main.py
+++ main.py
@@ -16,44 +16,47 @@ HEIGHT = 300
 
 
 def get_random_spooky_word():
+    '''returns a random word from words.txt'''
     with open("words.txt") as file:
         return random.choice(list(file))[:-1]
   
 
 def is_only_letters(word):
+    '''makes sure players' input is only letters'''
     for letter in word:
         if letter not in "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z":
          return False
     return True
 
-def word_place(spooky_word):
-    spooky_word = get_random_spooky_word
-    if len(spooky_word) != 
+#def word_place_clues(spooky_word, word_guess, guessed_letter):
+    #spooky_word = get_random_spooky_word
+
+    #word_placement = []
+        #for letter in guessed_letter:
+            #if guessed_letter[] == spooky_word
+                #return 
 
     
     
 
 
 
-print("this is hanging!")
+print("this is hangman")
+print("enter letters and guess word")
 
 
 while True:
 
-
-    spooky_word = get_random_spooky_word(num_letters)
-    print(f"spooky word is {spooky_word}")
-
+    spooky_word = get_random_spooky_word()
     num_letters = len(spooky_word)
-
-    while len(spooky_word) != int(num_letters):
-        new_spooky_word = get_random_spooky_word()
-        print(f"new spooky word is {spooky_word}")
-        break
+    print(f"spooky word is {spooky_word}")
 
     wrong_guesses_taken = 0
     while wrong_guesses_taken < max_wrong_guesses:
-        guess = ""
+        letter_guess = ""
+        while len(letter_guess) != int(max_wrong_guesses) or not is_only_letters:
+            letter_guess = input()
+
 
 
 
