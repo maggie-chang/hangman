407aaabb41a288277d220cba4d326dae8ded0e63
maggie <144709218+maggie-chang@users.noreply.github.com>
2023-10-20 16:20:58 +0000

    update man

--- main.py
+++ main.py
@@ -24,18 +24,13 @@ def get_random_spooky_word():
 def is_only_letters(word):
     """makes sure players' input is only letters"""
     for letter in word:
-        if (
-            letter
-            not in "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z"
-        ):
+        if (letter not in "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z"):
             return False
     return True
 
 
-def correct_word(
-    spooky_word,
-    guessed_word,
-):
+def correct_word(spooky_word, guessed_word):
+    '''returns "you won!" when the player correctly guesses the spooky word'''
     spooky_word = get_random_spooky_word
     if guessed_word == spooky_word:
         return print("you won!")
@@ -67,9 +62,16 @@ while True:
 
     wrong_guesses_taken = 0
     while wrong_guesses_taken < max_wrong_guesses:
-        letter_guess = ""
-        while len(letter_guess) != int(max_wrong_guesses) or not is_only_letters:
-            letter_guess = input()
+        guess = ""
+        while len(guess) != int(max_wrong_guesses) or not is_only_letters:
+            guess = input()
 
+
+
+    print(clues_from_letter(guess, spooky_word))
+    if correct_word == guess:
+        break
+    if guess >= max_wrong_guesses:
+        print("you killed the man...")
 
 pgzrun.go()
